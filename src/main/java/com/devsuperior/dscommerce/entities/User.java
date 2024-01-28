1f3a6d5a347ad71feac18d0d17dfd4e0c347cff3
José Guilherme <josegfernandes03@gmail.com>
2024-01-28 18:57:15 -0300

    classe Order criada e relacionamento com User feita

--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -3,6 +3,8 @@ package com.devsuperior.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name="tb_user")
@@ -16,9 +18,13 @@ public class User {
     private LocalDate birthDate;
     private String password;
 
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
+
     public User() {
 
     }
+
 
     public User(Long id, String name, String email, String phone, LocalDate birthDate, String password) {
         this.id = id;
@@ -75,5 +81,9 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 }
